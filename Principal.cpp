a0db8b755220f474b079f54ef7465c59da2846e5
mathaugust <matheus-augusto-11@hotmail.com>
2016-05-17 17:43:08 -0300

    Atualização 1
    Nova atualização.
    Criando um projeto no Github.
    Adicionando meu primeiro arquivo.

---
+++ Principal.cpp
@@ -0,0 +1,33 @@
+//meu primeiro código em c++
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* args[])
+{
+	char inicial;
+	string nome;
+	string classe;
+
+	cout << "digite o nome do personagem: " << endl;
+	getline(cin, nome);
+	inicial = nome[0];
+	cout << "digite a classe do personagem: " << endl;
+	getline(cin, classe);
+	cout << "personagem: " << inicial << ". " << nome.substr( nome.find(" ")) << endl;
+	cout << "classe: " << classe << endl;
+
+	int ataque = 10;
+	int defesa = 8, magica = 2, def_magica = 4;
+
+	int vida = 65, vida_maxima = 110;
+
+	cout << "status " << endl;
+	cout << "ataque : " << ataque << "defesa : " << defesa << endl;
+	cout << "magica : " << magica << "def. magica : " << def_magica << endl;
+
+	cout << "vida : " << vida << " / " << vida_maxima << endl;
+
+	return 0;
+
+}
